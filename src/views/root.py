7921a0220c3be010e9fab8c3f3ba74c33b8e4a0b
Caleb Ely <le717@users.noreply.github.com>
2019-07-04 15:00:11 -0400

    Update root.py

--- src/views/root.py
+++ src/views/root.py
@@ -24,9 +24,8 @@ def main() -> str:
         hook_config["service"]
     )(**hook_config)
     service.headers = dict(request.headers)
-    service.body = dict(request.data)
 #    service.set_body(request.data)
-#    service.body = request.data
+    service.body = request.data
 
     # Kick off the service process if authorized
     if service.is_authorized():
