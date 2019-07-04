505697643f8f2b4e77f8deabe719fefd9c93a89b
Caleb Ely <le717@users.noreply.github.com>
2019-07-04 13:12:04 -0400

    Working on GitHub auth

--- src/core/services/GitHub.py
+++ src/core/services/GitHub.py
@@ -1,21 +1,28 @@
 from dataclasses import dataclass
+from hashlib import sha1
+from json import dumps
 from os.path import abspath, expanduser
-
+import sys
 from src.core.services.base import Base
 
 
 @dataclass
 class GitHub(Base):
     def is_authorized(self) -> bool:
-        return False
+        digest = sha1(f"{self.expected_secret}{dumps(self.body)}").hexdigest()
+        signature = f"sha1={digest}"
+        sys.stdout.write(signature)
+        sys.stdout.write(self.headers[self._rewrite_header_key("HTTP_X_HUB_SIGNATURE")])
+        sys.stdout.write(str(signature == self.headers[self._rewrite_header_key("HTTP_X_HUB_SIGNATURE")]))
+        return signature == self.headers[self._rewrite_header_key("HTTP_X_HUB_SIGNATURE")]
 
     def main(self) -> bool:
         # Check if this is an authorized request
         # TODO Implement validation instead
         # https://developer.github.com/webhooks/securing/#validating-payloads-from-github
-        secret_key = self.body["config"]["secrets"]
-        if not self.is_authorized(secret_key):
-            return False
+#        secret_key = self.body["config"]["secrets"]
+#        if not self.is_authorized(secret_key):
+#            return False
 
         # Run any required commands before we `git pull`
         if self.before_pull:
