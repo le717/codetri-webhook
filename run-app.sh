6bcfca0725ef34cc8412392f2328047c7554fa5f
Caleb Ely <le717@users.noreply.github.com>
2021-08-19 12:02:31 -0400

    Add gunicorn logging

--- run-app.sh
+++ run-app.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn --bind 127.0.0.1:5006 --workers 2 wsgi:app
+gunicorn --bind 0.0.0.0:80 --workers 2 --log-level error --access-logfile /var/log/webhook.access.log --error-logfile /var/log/webhook.error.log wsgi:app
