7ac79f724d861bf573b7a8d1232d877067d93ef2
Caleb Ely <le717@users.noreply.github.com>
2021-08-18 21:03:05 -0400

    Remove header name rewtite method
    Why don't we just follow proper casing from the get-go...

--- src/core/services/base.py
+++ src/core/services/base.py
@@ -20,15 +20,6 @@ class Base:
     headers: Dict[str, str] = field(default_factory=dict)
     body: Dict[str, Any] = field(default_factory=dict)
 
-    @staticmethod
-    def _rewrite_header_key(key: str) -> str:
-        """Rewrite HEADER_NAME format to Header-Name format if needed."""
-        if "_" in key:
-            key = key.replace("_", "-")
-        if not key.istitle():
-            key = key.title()
-        return key
-
     def is_authorized(self) -> bool:
         raise NotImplementedError(
             "is_authorized() must be implemented by the child class!"
