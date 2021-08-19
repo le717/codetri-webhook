626426a743e5eee53d9cfa59631764618eb1c0f6
Caleb Ely <le717@users.noreply.github.com>
2021-08-18 21:04:58 -0400

    Rewrite hook config loading to use pathlib

--- src/core/app_config.py
+++ src/core/app_config.py
@@ -1,6 +1,6 @@
-from json import load
-from os import listdir
-from os.path import join
+from json import loads
+from pathlib import Path
+from typing import Dict, Generator, Union
 
 from dotenv import dotenv_values, find_dotenv
 
@@ -8,7 +8,7 @@ from dotenv import dotenv_values, find_dotenv
 __all__ = ["load_app_config", "load_hook_configs"]
 
 
-def load_app_config() -> dict:
+def load_app_config() -> Dict[str, Union[str, None]]:
     """Load the env variables from file."""
     vals = {}
     env_vals = dotenv_values(find_dotenv())
@@ -17,13 +17,7 @@ def load_app_config() -> dict:
     return vals
 
 
-def load_hook_configs():
-    # Find out what hooks have been defined
-    HOOKS_PATH = "hooks"
-    hooks = []
-    available_hooks = listdir(HOOKS_PATH)
-
-    # Load the available hooks
-    for hook_file in available_hooks:
-        with open(join(HOOKS_PATH, hook_file), "rt") as f:
-            yield load(f)
+def load_hook_configs() -> Generator[str, None, None]:
+    """Load all available hook configurations."""
+    for hook_file in Path("hooks").resolve().iterdir():
+        yield loads(hook_file.read_text())
