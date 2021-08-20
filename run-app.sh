7cca1d1cc82613521f3a35eb24d0b7a459d906a5
Caleb Ely <le717@users.noreply.github.com>
2021-08-19 20:21:17 -0400

    Chnage gunicorn log folder/files

--- run-app.sh
+++ run-app.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn --bind 127.0.0.1:6000 --workers 2 --log-level error --access-logfile /var/log/webhook.access.log --error-logfile /var/log/webhook.error.log wsgi:app
+gunicorn --bind 127.0.0.1:6000 --workers 2 --log-level error --access-logfile ./log/access.log --error-logfile ./log/error.log wsgi:app
