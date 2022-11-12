d3083bda063d66c7fc056ae3fdefc8640e8481bf
Caleb Ely <le717@users.noreply.github.com>
2022-11-12 14:28:50 -0500

    Use BIND_PORT file

--- run-app.sh
+++ run-app.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-env/bin/gunicorn --bind 127.0.0.1:5000 --workers 2 --log-level error --access-logfile ./log/gunicorn.access.log --error-logfile ./log/gunicorn.error.log wsgi:app
+# https://stackoverflow.com/a/3352015
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    echo "$var"
+}
+
+port=$(< ./secrets/BIND_PORT)"
+port="$(trim ${port})"
+env/bin/gunicorn --bind 127.0.0.1:${port} --workers 2 --log-level error --access-logfile ./log/gunicorn.access.log --error-logfile ./log/gunicorn.error.log wsgi:app
