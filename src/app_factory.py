5bbc2eaff8ca4f8c3a6f9ef492bb6b29b8e8209d
Caleb Ely <le717@users.noreply.github.com>
2022-11-12 21:00:46 -0500

    error handing improves

--- src/app_factory.py
+++ src/app_factory.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-from flask import Flask, Response, abort
+from flask import Flask, Response
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from src.core import config, logger
@@ -9,7 +9,13 @@ from src.views import root
 
 def create_app():
     app = Flask(__name__)
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app,
+        x_for=1,
+        x_proto=1,
+        x_host=1,
+        x_prefix=1,
+    )
 
     # Load the app configuration
     app.config.update(config.app("default"))
@@ -29,25 +35,23 @@ def create_app():
         # Create an endpoint for each hook
         app.config["SUPPORTED_HOOKS"].update({hook["name"]: hook})
         root.bp.add_url_rule(
-            f"/{hook['name']}".lower(), hook["name"], root.main, methods=["POST"]
+            f"/{hook['name']}".lower(),
+            hook["name"],
+            root.main,
+            methods=["POST"],
         )
 
     # Register the blueprint
     app.register_blueprint(root.bp)
 
-    # All access to undefined routes are bad requests
     @app.errorhandler(404)
     def not_found_handler(e) -> Response:
-        abort(400)
-
-    # TODO: write me
-    @app.errorhandler(405)
-    def method_not_allowed_handler(e) -> Response:
+        """All access to undefined routes are bad requests."""
         return {}, 400
 
-    # TODO: write me
-    @app.errorhandler(500)
-    def server_error_handler(e) -> Response:
+    @app.errorhandler(405)
+    def method_not_allowed_handler(e) -> Response:
+        """Only POST requests are allowed."""
         return {}, 400
 
     return app
