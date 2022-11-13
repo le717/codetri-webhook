fe0d5754981db9076b19e4327a21b66a20654eda
Caleb Ely <le717@users.noreply.github.com>
2022-11-12 21:07:17 -0500

    Add sample hook/service

---
+++ samples/services/Sample.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+from src.core.services.base import Base
+
+
+@dataclass
+class Sample(Base):
+    def is_authorized(self) -> bool:
+        """Check if this is an authorized request."""
+        secret_key = self.headers.get("X-Sample-Token", "")
+        return secret_key == self.secret
+
+    def main(self) -> bool:
+        # Run any required commands before we `git pull`
+        if not self.run_commands(self.before_pull):
+            return False
+
+        # Run any required commands after the `git pull`
+        if not self.run_commands(self.after_pull):
+            return False
+
+        # Everything worked! Woo! :D
+        return True
