0eed473813f6e32fb9cf66efcb3c774649e3ff9a
Caleb Ely <le717@users.noreply.github.com>
2022-11-14 13:06:21 -0500

    CopY getting bind port into stop.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
+trim() {
+    # https://stackoverflow.com/a/3352015
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    echo "$var"
+}
+
+# Get the bind port
+webhook_bind_port="$(< ./secrets/BIND_PORT)"
+webhook_bind_port="$(trim ${port})"
 
 # Stop the supervisord process
 # TODO: Do this!
 
 # Stop the gunicorn processes
-lsof -t -i:$webhook_bind_port | xargs kill -9
+lsof -t -i :$webhook_bind_port | xargs kill -9
