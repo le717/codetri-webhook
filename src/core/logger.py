5043ffebadf489338ff5ff66a10af975daa08521
Caleb Ely <le717@users.noreply.github.com>
2022-11-16 14:08:12 -0500

    Try to fix logging

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,20 +1,19 @@
 import logging
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
-from sys import stdout
 
 
 __all__ = ["LOG", "file_handler"]
 
 
-LOG = logging.getLogger("codetri-webhook")
+LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.ERROR)
 
 
 def file_handler(log_name: str) -> RotatingFileHandler:
     """Create a file-based error handler."""
     handler = RotatingFileHandler(
-        Path("log") / log_name,
+        (Path("log") / "app" / log_name).as_posix(),
         maxBytes=500_000,
         backupCount=5,
         delay=True,
