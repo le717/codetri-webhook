90f15b11149a4bc05b039096a8560bf93e1d9e8b
Caleb Ely <le717@users.noreply.github.com>
2024-10-21 21:27:38 -0400

    fix path to scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,8 +7,8 @@ python3 -m venv venv
 
 # Install the app deps
 venv/bin/python -m pip install pip --upgrade
-venv/bin/python ./get-requirements.py
+venv/bin/python ./scripts/get-requirements.py
 venv/bin/pip install -r ./requirements.txt
 
 # Make the scripts executable
-chmod u+x ./start.sh ./stop.sh ./run-app.sh
+chmod u+x ./scripts/start.sh ./scripts/stop.sh ./scripts/run-app.sh
